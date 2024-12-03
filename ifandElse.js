9ee7ee73b5a8c6e86253240303852b197584a66f
Salomao <augusto@salomaotech.com.br>
2024-12-03 07:42:26 -0300

    Começando um novo estudo

---
+++ ifandElse.js
@@ -0,0 +1,12 @@
+let avaliação = 3
+
+if(avaliação === 1){
+    console.log("Mau Resultado");
+    
+} else if(avaliação === 2){
+    console.log("Resultado ok");
+    
+}else {
+    console.log("Resultado positivo");
+    
+}
